94786f2178371b7afd55c3bf347f2ecfece8e48f
Karl F <karlfogel@gmail.com>
2019-09-07 19:46:19 +0200

    My initial keyboard / keycode layout
    Initially, just for testing, loosely modeled after Kinesis advantage

--- keyboards/handwired/dactyl_impaktor/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_impaktor/4x6/keymaps/default/keymap.c
@@ -26,36 +26,34 @@ extern keymap_config_t keymap_config;
 #define LOWER MO(_LOWER)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
 /* Base (qwerty)
  * +-----------------------------------------+                             +-----------------------------------------+
- * | ESC  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |      |
+ * | ESC  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |  -   |
  * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | TAB  |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |      |
+ * |LCTRL |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |  '   |
  * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
- * | SHFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |      |
+ * |LSHFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  | \    |
  * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
- *               |  [   |   ]  |                                                         |      |      |
- *               +-------------+-------------+                             +-------------+-------------+
- *                             |      |      |                             |      |      |
- *                             |------+------|                             |------+------|
- *                             |      |      |                             |      |      |
- *                             +-------------+                             +-------------+
- *                                           +-------------+ +-------------+
- *                                           |      |      | |      |      |
- *                                           |------+------| |------+------|
- *                                           |      |      | |      |      |
- *                                           +-------------+ +-------------+
+ *               |  [   |   ]  |                                                         |lower |raise |
+ *               +-------------+   +-----+-----+                         +-----------+   +-------------+
+ *                                 |BKSP |LALT |                         | RET | SPC |
+ *                                 |     |     |                         |     |     |
+ *                                 +-----+-----+                         +-----+-----+
+ *                                      +-------------+           +-------------+
+ *                                      | HOME | WIN  |           | PGUP | rALT |
+ *                                      |------+------|           |------+------|
+ *                                      | END  | ` ~  |           | PGDN |RCTRL |
+ *                                      +-------------+           +-------------+
  */
 
 [_BASE] = LAYOUT( \
-    KC_ESC,  KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,   \
-    KC_TAB,  KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,   \
-    KC_LSFT, KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSLASH, \
-                    KC_LBRC,KC_RBRC,                                            KC_PLUS,KC_EQL,                    \
-                                    RAISE,  KC_SPC,             KC_ENT, LOWER,                                     \
-                                    KC_TAB, KC_HOME,            KC_END, KC_DEL,                                    \
-                                    KC_BSPC,KC_GRV,             KC_LGUI,KC_LALT                                    \
+    KC_ESC,   KC_Q,  KC_W,   KC_E,   KC_R,   KC_T,               KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,   \
+    KC_LCTRL, KC_A,  KC_S,   KC_D,   KC_F,   KC_G,               KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,   \
+    KC_LSFT,  KC_Z,  KC_X,   KC_C,   KC_V,   KC_B,               KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSLASH, \
+                     KC_LBRC,KC_RBRC,                                            LOWER,  RAISE,                     \
+                                    KC_BSPC, KC_LALT,          KC_ENT,  KC_SPC,                                     \
+                                    KC_HOME, KC_LGUI,          KC_PGUP, KC_RALT,                                    \
+                                    KC_END,  KC_GRV,           KC_PGDN, KC_RCTRL                                    \
 ),
 
 [_LOWER] = LAYOUT(
