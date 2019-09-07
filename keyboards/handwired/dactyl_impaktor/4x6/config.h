5ff3c3ca1c3ddbdee299aa29aa7099b00d8c5e3d
Karl F <karlfogel@gmail.com>
2019-09-07 18:44:50 +0200

    Add myself as author
    All glory to me, MEEE!

--- keyboards/handwired/dactyl_impaktor/4x6/config.h
+++ keyboards/handwired/dactyl_impaktor/4x6/config.h
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x3060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    tshort
+#define MANUFACTURER    impaktor
 #define DESCRIPTION     A split keyboard for the cheap makers
 
 /* key matrix size */
